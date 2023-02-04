89ae41fd1d23bf8bc7561564c811c12df6386b97
me <mycloud422@ya.ru>
2023-02-05 01:27:12 +0300

    Lesson-1.complete

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,13 @@
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import { classNames } from './helpers/classNames/classNames';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
+
+import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 import './styles/index.scss';
-import { useTheme } from './theme/useTheme';
+
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+
+import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
 
@@ -17,8 +20,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import { FC, useMemo, useState } from 'react'
-import { LOCALE_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
+import { LOCALE_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext'
 
 const defaultTheme = localStorage.getItem(LOCALE_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
