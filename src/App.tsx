fffcd8f0815db0d300b13b1f11f36ddbaee82f05
me <mycloud422@ya.ru>
2023-02-04 21:41:03 +0300

    Lesson-1.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import { AboutPageAsync } from './components/pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './components/pages/MainPage/MainPage.async';
-import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
-    return (
-        <div className="app">
 
+    const { theme, toggleTheme } = useTheme()
+
+    return (
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
