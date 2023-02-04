895e2b517032211c7c743d76b041bac1aaf0bd9e
me <mycloud422@ya.ru>
2023-02-04 16:19:20 +0300

    lesson1.5

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,28 +1,28 @@
-import webpack from 'webpack';
-import { buildLoaders, buildPlugins, buildResolvers } from './';
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import path from "path";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
 
-import { BuildOptions } from 'config/build/types/config';
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode, isDev} = options;
 
-export function buildWebpackConfig(
-	options: BuildOptions
-): webpack.Configuration {
-	const { mode, paths } = options;
-
-	return {
-		mode,
-		entry: paths.entry,
-		output: {
-			filename: "[name].[contenthash].js",
-			path: paths.build,
-			clean: true,
-		},
-		plugins: buildPlugins(options),
-		module: {
-			// правила работы с разными расширениями 
-			rules: buildLoaders(),
-		},
-		resolve: buildResolvers(),
-		// для стэктрейса 
-		devtool: 'inline-source-map',
-	};
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    }
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,21 +1,28 @@
-import path from 'path';
 import webpack from 'webpack';
-import { buildWebpackConfig } from './config/build';
-import { BuildPath } from './config/build/types/config';
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildEnv, BuildPaths} from "./config/build/types/config";
+import path from "path";
 
-const paths: BuildPath = {
-	build: path.resolve(__dirname, 'build'),
-	entry: path.resolve(__dirname, 'src', 'index.ts'),
-	html: path.resolve(__dirname, 'public', 'index.html'),
+
+export default (env: BuildEnv) => {
+
+
+    const paths: BuildPaths = {
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html'),
+    }
+
+    const mode = env.mode || 'development';
+    const isDev = mode === 'development';
+    const PORT = env.port || 3000;
+
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT,
+    })
+
+    return config;
 };
-
-const mode = 'development';
-const isDev = mode === 'development';
-
-const config: webpack.Configuration = buildWebpackConfig({
-	mode,
-	paths,
-	isDev,
-});
-
-export default config;
