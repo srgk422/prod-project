6114a461864ff7b6bd2654bfa55787cd052d7f21
me <mycloud422@ya.ru>
2023-02-04 22:19:41 +0300

    Lesson-1.8 classNames helper added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+import { classNames } from './helpers/classNames/classNames';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import './styles/index.scss';
@@ -10,7 +11,7 @@ const App = () => {
     const { theme, toggleTheme } = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,13 @@
+type Mods = Record<string, boolean | string>
+
+export function classNames(acl: string, mods: Mods, additional: string[]): string {
+  return [
+    acl,
+    ...Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className]) => className),
+    ...additional,
+  ].join(' ');
+
+}
+
