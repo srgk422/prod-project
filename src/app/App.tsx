52c7c41d763b4d93fe2548101011f9d9b6610c0f
me <mycloud422@ya.ru>
2023-02-27 00:28:57 +0300

    04.03 fix Theme styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,18 @@
 import { Suspense } from 'react';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router/ui';
 
-const App = () => {
-  const { theme } = useTheme();
-
-  return (
-    <div className={classNames('app', {}, [theme])}>
-      <Suspense fallback="">
-        <Navbar />
-        <div className="content-page">
-          <Sidebar />
-          <AppRouter />
-        </div>
-      </Suspense>
-    </div>
-  );
-};
-
+const App = () => (
+  <div className={classNames('app', {}, [])}>
+    <Suspense fallback="">
+      <Navbar />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
+    </Suspense>
+  </div>
+);
 export default App;
