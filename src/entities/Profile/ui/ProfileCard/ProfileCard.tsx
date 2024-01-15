6cbe78c6c55aa4104e8d45e7edfec3bfd3576abd
me <mycloud422@ya.ru>
2024-01-15 23:33:18 +0300

    enable pre-commit actions, update loki snaps, fix storybook config

--- src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.tsx
@@ -3,7 +3,7 @@ import cls from './ProfileCard.module.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { getProfileData, getProfileError, getProfileIsLoading } from '../../model/selectors';
+import { getProfileData } from '../../model/selectors';
 import { Text } from 'shared/ui/Text/Text';
 import { Button, ButtonDesign } from 'shared/ui/Button';
 import { Input } from 'shared/ui/Input/Input';
@@ -18,8 +18,6 @@ export const ProfileCard: FC<ProfileCardProps> = (props) => {
   const { t } = useTranslation('profile');
 
   const data = useSelector(getProfileData);
-  const isLoading = useSelector(getProfileIsLoading);
-  const error = useSelector(getProfileError);
 
   return (
     <div className={classNames(cls.ProfileCard, {}, [className])}>
